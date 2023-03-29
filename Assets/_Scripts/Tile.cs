c24c40946fec4cbd416661c1550d84e9b89dea13
Jose Cabrera <murdoc.jose.6@gmail.com>
2023-03-29 12:40:16 -0300

    feat: done merging

--- Assets/_Scripts/Tile.cs
+++ Assets/_Scripts/Tile.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -37,7 +38,7 @@ public class Tile : MonoBehaviour
         transform.position = cell.transform.position;
     }
 
-    public void Move(TileCell cell)
+    public void MoveTo(TileCell cell)
     {
         if (this.cell != null)
         {
@@ -46,6 +47,39 @@ public class Tile : MonoBehaviour
         this.cell = cell;
         this.cell.tile = this;
 
-        transform.position = cell.transform.position;
+        StartCoroutine(Animate(cell.transform.position, false));
+    }
+
+    public void Merge(TileCell cell)
+    {
+        if (this.cell != null)
+        {
+            this.cell.tile = null;
+        }
+
+        this.cell = null;
+        StartCoroutine(Animate(cell.transform.position, true));
+    }
+
+    private IEnumerator Animate(Vector3 to, bool merging)
+    {
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+
+        while (elapsed < duration)
+        {
+            transform.position = Vector3.Lerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = to;
+
+        if (merging)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/_Scripts/TileBoard.cs
+++ Assets/_Scripts/TileBoard.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class TileBoard : MonoBehaviour
     public TileState[] tileStates;
     private TileGrid grid;
     private List<Tile> tiles;
+    private bool waiting;
 
     void Awake()
     {
@@ -31,26 +33,30 @@ public class TileBoard : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (!waiting)
         {
-            MoveTile(Vector2Int.up, 0, 1, 1, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveTile(Vector2Int.down, 0, 1, grid.height - 2, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveTile(Vector2Int.left, 1, 1, 0, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            MoveTile(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTiles(Vector2Int.up, 0, 1, 1, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTiles(Vector2Int.down, 0, 1, grid.height - 2, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTiles(Vector2Int.left, 1, 1, 0, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTiles(Vector2Int.right, grid.width - 2, -1, 0, 1);
+            }
         }
     }
 
-    void MoveTile(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
+    void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
     {
+        bool changed = false;
         for (int x = startX; x >= 0 && x < grid.width; x += incrementX)
         {
             for (int y = startY; y >= 0 && y < grid.height; y += incrementY)
@@ -59,13 +65,17 @@ public class TileBoard : MonoBehaviour
 
                 if (cell.occupied)
                 {
-                    MoveTile(cell.tile, direction);
+                    changed |= MoveTile(cell.tile, direction);
                 }
             }
         }
+        if (changed)
+        {
+            StartCoroutine(WaitForChanges());
+        }
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         TileCell newCell = null;
         TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);
@@ -73,7 +83,11 @@ public class TileBoard : MonoBehaviour
         {
             if (adjacent.occupied)
             {
-                // TODO: merging
+                if (CanMerge(tile, adjacent.tile))
+                {
+                    Merge(tile, adjacent.tile);
+                    return true;
+                }
                 break;
             }
             newCell = adjacent;
@@ -81,8 +95,55 @@ public class TileBoard : MonoBehaviour
         }
         if (newCell != null)
         {
-            tile.Move(newCell);
+            tile.MoveTo(newCell);
+            return true;
         }
+
+        return false;
+    }
+
+    private bool CanMerge(Tile a, Tile b)
+    {
+        return a.number == b.number;
+    }
+
+    private void Merge(Tile a, Tile b)
+    {
+        tiles.Remove(a);
+        a.Merge(b.cell);
+
+        int index = Mathf.Clamp(IndexOf(b.state) + 1, 0, tileStates.Length - 1);
+        int number = b.number * 2;
+
+        b.SetState(tileStates[index], number);
+
+    }
+
+    private int IndexOf(TileState state)
+    {
+        for (int i = 0; i < tileStates.Length; i++)
+        {
+            if (state == tileStates[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private IEnumerator WaitForChanges()
+    {
+        waiting = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        waiting = false;
+
+        if (tiles.Count != grid.size)
+        {
+            CreateTile();
+        }
+        // TODO: check for game over
     }
 
 }
